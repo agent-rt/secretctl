7ce0244a96fa9b88c340b904e4d85439786b117d
linguofeng <linguofeng@msn.com>
2026-06-24 15:40:45 +0900

    chore: release v0.6.0

--- src/cli.zig
+++ src/cli.zig
@@ -78,7 +78,7 @@ pub fn run(allocator: std.mem.Allocator, args: []const []const u8) u8 {
         return 0;
     }
     if (std.mem.eql(u8, cmd, "--version")) {
-        tty.writeStdout("secretctl 0.5.1\n");
+        tty.writeStdout("secretctl 0.6.0\n");
         return 0;
     }
 
